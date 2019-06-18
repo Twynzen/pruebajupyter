2aa3559c7217a3e037d742cab3bf857279d39e26
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2019-06-18 14:58:00 -0500

    librerias python
    Librerias importantes para trabajar data ciencia y redes neuronales eventualemente

---
+++ Untitled1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import  numpy as np #Calculo nùmerico, funcionalidades con arrays, matrices operaciones de convertir matriz transpuesta etc...
+import scipy as sc  #Lo mismo que numpy pero agrega màs funcionalidades de tratamiento de imagenes y datos
+
+
+import matplotlib.pyplot as plt # libreria de visualizacion grafica
+
